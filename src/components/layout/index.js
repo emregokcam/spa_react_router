b36b0bf7bea0b8021eed30f81a66cdf376c03c5e
E2361Emre <67741583+E2361Emre@users.noreply.github.com>
2021-05-04 18:25:18 +0300

    added pages and layout

---
+++ src/components/layout/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Layout = () => <div>This is the LAYOUT!</div>;
+
+export { Layout };
---
+++ src/components/pages/Contact.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Contact = () => <div>This is the Contact page;</div>;
+
+export default Contact;
